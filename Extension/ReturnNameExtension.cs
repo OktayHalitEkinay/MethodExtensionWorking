db50aad64feb7b55ac1727f9ebfc73ac49f42ec4
OktayHalitEkinay <67113059+OktayHalitEkinay@users.noreply.github.com>
2021-09-29 13:37:11 +0300

    Add project files.

---
+++ Extension/ReturnNameExtension.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleUI.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.Extension
+{
+    public static class ReturnNameExtension
+    {
+        public static string ReturnNameSurname(this EntityManager entityManager,string lastName)
+        {
+            return entityManager.ReturnName()+" "+ lastName;         
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleUI.Concrete;
+using ConsoleUI.Extension;
+using System;
+
+namespace MethodExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntityManager entityManager = new EntityManager();
+            Console.WriteLine(entityManager.ReturnNameSurname("EKINAY")); 
+        }
+    }
+}
